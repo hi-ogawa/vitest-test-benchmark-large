3ad57397eaa806f3f9092cc7139aec1b67b5fca7
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-09-23 14:22:32 +0900

    chore: tweak

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import v8 from "node:v8";
 
 export default defineConfig({});
 
-// log heap stats
-(async () => {
-  while (true) {
-    await new Promise((r) => setTimeout(r, 1000));
-    const stats = v8.getHeapStatistics();
-    fs.appendFileSync("./heap.log", `${stats.used_heap_size}\n`);
-  }
-})();
+if (process.env.DEBUG_DUMP_HEAP) {
+  (async () => {
+    while (true) {
+      await new Promise((r) => setTimeout(r, 1000));
+      const stats = v8.getHeapStatistics();
+      fs.appendFileSync("./heap.log", `${stats.used_heap_size}\n`);
+    }
+  })();
+}
